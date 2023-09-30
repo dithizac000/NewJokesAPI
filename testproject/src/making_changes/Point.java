14bcd7eb3a573a5f9840a586c530c19fed69812f
dithizac000 <dithizac000@gmail.com>
2023-09-30 00:19:12 -0700

    revert back to original point class

--- testproject/src/making_changes/Point.java
+++ testproject/src/making_changes/Point.java
@@ -1,53 +1,7 @@
 package making_changes;
 
-import java.util.Objects;
-
 public class Point {
     private double x;
     private double y;
-
-    public Point(double x, double y) {
-        this.x = x;
-        this.y = y;
-    }
-
-    public double getX() {
-        return x;
-    }
-
-    public void setX(double x) {
-        this.x = x;
-    }
-
-    public double getY() {
-        return y;
-    }
-
-    public void setY(double y) {
-        this.y = y;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (!(o instanceof Point point)) {
-            return false;
-        }
-        return Double.compare(getX(), point.getX()) == 0 && Double.compare(getY(), point.getY()) == 0;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(getX(), getY());
-    }
-
-    @Override
-    public String toString() {
-        return "Point{" +
-                "x=" + x +
-                ", y=" + y +
-                '}';
-    }
+    
 }
